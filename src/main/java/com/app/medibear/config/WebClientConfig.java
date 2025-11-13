1d6b10532a3db47ab6d8a5454751a4ad95d45f78
Hayden721 <hayden00721@gmail.com>
2025-11-13 14:51:21 +0900

    칼로리 예측

--- src/main/java/com/app/medibear/config/WebClientConfig.java
+++ src/main/java/com/app/medibear/config/WebClientConfig.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+
 
 @Configuration
 public class WebClientConfig {
@@ -15,6 +18,7 @@ public class WebClientConfig {
   public WebClient webClient() {
     return WebClient.builder()
       .baseUrl(corsFastApiUrl)
+      .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
       .build();
   }
 
